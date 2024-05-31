230f9bd939850f596fa6bdc8788be7ddf9a7d2e1
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-05-31 19:31:49 -0400

    created a library, adding books, removing and returning the books and listing them again

--- library.js
+++ library.js
@@ -2,7 +2,7 @@ class Book {
     constructor(title, author, isbn, availableCopies){
         this.title = title;
         this.author = author;
-        this.isbn;
+        this.isbn = isbn;
         this.availableCopies = availableCopies;
     }
 
@@ -22,4 +22,44 @@ class Book {
     }
 }
 
+returnBook(){
+    this.availableCopies++;
+    console.log(`"${this.title}" returned successfully.`);
+   }
 
+   class Library {
+    constructor(name){
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        this.books = this.books.filter((book) => book.isbn !== isbn);
+    }
+
+    listAllBooks() {
+        console.log(`** ${this.name} Book List **`);
+        for (const book of this.books) {
+          console.log(`- Title: ${book.title}, Author: ${book.author}, ISBN: ${book.isbn}, Available Copies: ${book.availableCopies}`);
+        }
+      }
+    }
+
+    const myLibrary = new Library("Read with Tiffany");
+
+ const book1 = new Book("The Berenstain Bears in the Dark", "Stan and Jan Berenstain", "9780261102694", 2);
+ const book2 = new Book("Cat in the Hat", "Dr. Seuss", "9780394800011", 17);
+
+ myLibrary.addBook(book1);
+ myLibrary.addBook(book2);
+
+ book1.borrowBook(); 
+ book2.borrowBook(); 
+
+ myLibrary.listAllBooks();
+
+ myLibrary.removeBook(book2.isbn);
+ book2.returnBook(); 
+
+ myLibrary.listAllBooks();
+ 
