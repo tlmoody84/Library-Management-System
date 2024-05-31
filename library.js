1503fedd8cf8450d2a594b4918a1adc800811b1a
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-05-31 18:28:38 -0400

    created a book classwith multiple properties and added a constructor class method that takes in arguments

---
+++ library.js
@@ -0,0 +1,19 @@
+class Book {
+    constructor(title, author, isbn, availableCopies){
+        this.title = title;
+        this.author = author;
+        this.isbn;
+        this.availableCopies = availableCopies;
+    }
+
+    borrowBook() {
+        if (this.availableCopies > 0) {
+            this.availableCopies--;
+            console.log(`"${this.title}" borrowed successfully.`);
+
+        }else {
+            console.log(`Sorry, no copies of"${this.title}" are available.`);
+        }
+    }
+
+}
